1f1b7eaca4821da931676ab8315d2a6d06e934e6
Misty De Meo <mistydemeo@gmail.com>
2016-01-06 13:50:21 -0400

    Header: allow components to be removed

--- app/header/header.controller.js
+++ app/header/header.controller.js
@@ -6,6 +6,12 @@ class HeaderController {
   constructor(Transfer) {
     this.transfer = Transfer;
   }
+
+  remove_component(component) {
+    if (confirm(`Are you sure you want to remove this transfer component (${component.path})?`)) {
+      this.transfer.components.pop(component);
+    }
+  }
 }
 
 export default angular.module('controllers.header', ['services.transfer']).

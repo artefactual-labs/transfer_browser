07f9aa7e8de819a8cfb60e85394ec17dd325bf11
Misty De Meo <mistydemeo@gmail.com>
2016-01-06 13:50:47 -0400

    Header: add metadata editing

--- app/header/header.controller.js
+++ app/header/header.controller.js
@@ -7,6 +7,25 @@ class HeaderController {
     this.transfer = Transfer;
   }
 
+  add_next_metadata() {
+    let element = {};
+    this.transfer.fetch_id_for(element).then(() => {
+      this.transfer.next_id = element.id;
+      window.open(`/transfer/component/${element.id}`, '_blank');
+    });
+  }
+
+  open_edit_page(component) {
+    let open_window = () => {
+      window.open(`/transfer/component/${component.id}`, '_blank');
+    };
+    if (!component.id) {
+      this.transfer.fetch_id_for(component).then(open_window);
+    } else {
+      open_window();
+    }
+  }
+
   remove_component(component) {
     if (confirm(`Are you sure you want to remove this transfer component (${component.path})?`)) {
       this.transfer.components.pop(component);

53f49378eb2ecd4960703bf39c5f6dbdc2c796ce
Misty De Meo <mistydemeo@gmail.com>
2016-02-29 16:50:49 -0800

    Browse service: add comments

--- app/services/browse.service.js
+++ app/services/browse.service.js
@@ -5,11 +5,17 @@ import 'restangular';
 
 import {decode_browse_response, format_entries} from 'archivematica-browse-helpers';
 
+// Interacts with the Archivematica API to allow transfer source locations to be browsed.
 class Browse {
   constructor(Restangular) {
     this.browser = Restangular.one('filesystem').one('children').one('location');
   }
 
+  // Browses the contents of `path` at `location_uuid`.
+  // * location_uuid - The UUID of the location in the given Storage Service instance.
+  // * path - The directory whose contents should be retrieved.
+  //
+  // Returns a promise which resolves to an array of objects formatted in the format defined by archivematica-browse-helpers.
   browse(location_uuid, path) {
     let params = {'path': Base64.encode(path)};
     return this.browser.one(location_uuid).get(params).then(decode_browse_response).then(response => {

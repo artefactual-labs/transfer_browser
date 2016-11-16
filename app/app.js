244c6fedec87e42acfbff98e188f3a97b60ffaf8
Jesús García Crespo <jesus@sevein.com>
2016-11-15 20:32:09 -0800

    Add i18n support

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import 'angular-gettext';
 import 'angular-tree-control';
 // FIXME: doing this clashes with the dashboard's global bootstrap;
 //        we should find a better fix for this at some point.
@@ -15,6 +16,7 @@ import './browse/browse.controller';
 import './header/header.controller';
 
 export default angular.module('transferBrowse', [
+  'gettext',
   'restangular',
   'treeControl',
   'services.browse',
@@ -23,6 +25,22 @@ export default angular.module('transferBrowse', [
   'controllers.browse',
   'controllers.header',
 ]).
+
+run(function ($window, gettextCatalog) {
+  // Look up current language, fallback to English
+  var currentLanguage;
+  try {
+      currentLanguage = $window.DashboarConfig.currentLanguage
+  } catch (err) {
+      currentLanguage = 'en';
+  }
+  gettextCatalog.setCurrentLanguage(currentLanguage);
+
+  // Load translations
+  for (let [langCode, translations] of Object.entries(require('json!./locale/translations.json'))) {
+      gettextCatalog.setStrings(langCode, translations);
+  }
+}).
 
 config(['RestangularProvider', function(RestangularProvider) {
   RestangularProvider.setRequestSuffix('/');

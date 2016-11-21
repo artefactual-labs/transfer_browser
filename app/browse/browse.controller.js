e43d2fdbb253693da2fb7e8829c546224a441a7d
Holly Becker <hbecker@artefactual.com>
2016-11-21 14:08:16 -0800

    Browser: Fix DSpace transfer to accept zip files or directories
    DSpace transfer accepts compressed files or directories. Fix regression in
    transfer-browser to allow zip files to be selected.

--- app/browse/browse.controller.js
+++ app/browse/browse.controller.js
@@ -63,11 +63,25 @@ class BrowseController {
     });
   }
 
+  zip_filter(file) {
+      // Check if it's a compressed file
+      if (
+        file.title.endsWith('.zip') ||
+        file.title.endsWith('.tgz') ||
+        file.title.endsWith('.tar.gz')
+      ) {
+          return true;
+      }
+      return false;
+    }
+
   // Determines whether a given file can be added to the transfer,
   // depending on the transfer type.
   file_can_be_added(file) {
     if (this.transfer.type === 'zipped bag') {
-      return !file.directory && (file.title.endsWith('.zip') || file.title.endsWith('.tgz') || file.title.endsWith('.tar.gz'));
+      return !file.directory && this.zip_filter(file);
+    } else if (this.transfer.type === 'dspace') {
+      return file.directory || this.zip_filter(file);
     } else {
       return file.directory;
     }
